05148fef1825fe19c06465c3d9815b370dbfecb4
paulodebian <paulodebian@localhost.localdomain>
2024-03-11 06:59:23 -0400

    fix typo

--- functions/generate_matrix.c
+++ functions/generate_matrix.c
@@ -10,7 +10,7 @@ double *generate_matrix_double (int rows, int columns) {
 
     for (int i = 0; i < rows; i++) 
 	for (int j = 0; j < columns; j++) 
-	    matrix[i * columns +j] = (double) (rand()) / RAND_MAX * DOUBLE;    
+	    matrix[i * columns + j] = (double) (rand()) / RAND_MAX * DOUBLE;    
 
      return matrix;
 
