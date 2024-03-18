0250226a0358d7521fb54331581465a089b44f34
paulolvsq <paulantoinelevesque@gmail.com>
2024-03-18 22:52:05 +0100

    add QR decomposition with modified Gram Schmidt method

--- functions/QR_decomposition.c
+++ functions/QR_decomposition.c
@@ -5,6 +5,8 @@ QR *create_QR(double *A, int rows, int columns) {
     QR *QR_decomposition = malloc(sizeof(QR));
 
     // A (rows * columns) ||| Q (rows * rows) ||| R (rows * columns)
+    // A (m * n) ||| Q (m * m) ||| R (m * n)
+    // m > n with rank(A) = n
 
     QR_decomposition->rows = rows;
     QR_decomposition->columns = columns;
@@ -15,4 +17,36 @@ QR *create_QR(double *A, int rows, int columns) {
 
     return QR_decomposition;
 
+}
+
+QR *QR_decomposition(double *A, int rows, int columns) {
+
+    QR *QR_decomposition = create_QR(A, rows, columns);
+
+    // ASSERTION : THE USER WILL ALWAYS GIVE MATRICES OF THE RIGHT SIZE AS INPUT
+    // MODIFIED GRAM SCHMIDT METHOD
+
+    for (int k = 0; k < columns; k++) {
+	double s = 0.0;
+	for (int j = 0; j < rows; j++) {
+	    s += pow(QR_decomposition->A[j * columns + k], 2);
+	}
+	QR_decomposition->R[k * columns + k] = sqrt(s);
+	for (int j = 0; j < rows; j++) {
+	    QR_decomposition->Q[j * columns + k] = QR_decomposition->A[j * columns + k] / QR_decomposition->R[k * columns + k];
+	}
+	for (int i = k + 1; i < columns; i++) {
+	    s = 0.0;
+	    for (int j = 0; j < rows; j++) {
+		s += QR_decomposition->A[j * columns + i] * QR_decomposition->Q[j * columns + k];
+	    }
+	    QR_decomposition->R[k * columns + i] = s;
+	    for (int j = 0; j < rows; j++) {
+		QR_decomposition->A[j * columns + i] = QR_decomposition->A[j * columns + i] - QR_decomposition->R[k * columns + i] * QR_decomposition->Q[j * columns + k];
+	    }
+	}
+    }
+
+    return QR_decomposition;
+    
 }
---
+++ tests/TEST_QR_decomposition.c
@@ -0,0 +1,37 @@
+#include "LinearAlgebraBasics.h"
+
+int main() {
+
+    printf("##################################### TEST QR DECOMPOSITION #####################################\n");
+    
+    int rows = 3, columns = 3;
+    double A[] = {12.0, -51.0, 4.0, 6.0, 167.0, -68.0, -4.0, 24.0, -41.0};
+
+    QR *QR_test = QR_decomposition(A, rows, columns);
+
+    printf("Q =\n");
+
+    for (int i = 0; i < rows; i++) {
+	for (int j = 0; j < columns; j++) {
+	    printf("%lf\t", QR_test->Q[i * columns + j]);
+	}
+	printf("\n");
+    }
+
+    printf("R =\n");
+
+    for (int i = 0; i < rows; i++) {
+	for (int j = 0; j < columns; j++) {
+	    printf("%lf\t", QR_test->R[i * columns + j]);
+	}
+	printf("\n");
+    }
+
+    free(QR_test->Q);
+    free(QR_test->R);
+
+    free(QR_test);
+
+    return 0;
+
+}
